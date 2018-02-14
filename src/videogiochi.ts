6b56a8997584a7b04fa35abc92fe8d73f77d93f4
fiacca98 <lui.gi98@live.it>
2018-02-14 10:11:48 +0100

    creazione classe videogiochi e navbar

---
+++ src/videogiochi.ts
@@ -0,0 +1,12 @@
+export class Videogiochi{
+    console: string;
+    name: string;
+    price: number;
+
+    constructor(console: string = "PS4", name: string = "Topolino", price: number = 100.50){
+        this.console=console;
+        this.name=name;
+        this.price=price;
+    }
+    
+}
